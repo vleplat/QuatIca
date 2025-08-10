eda96f5a0bc3e7ce8b264e8dd6b78585eb3e0437
vleplat <valentin.leplat@gmail.com>
2025-08-10 09:36:46 -0700

    Docs/Notebook: clarify seaborn dependency for Visualizer; revert import guards; keep core package exports stable

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,17 @@
 from .solver import NewtonSchulzPseudoinverse, HigherOrderNewtonSchulzPseudoinverse, QGMRESSolver
 from .utils import *
 from .data_gen import *
+from .visualization import Visualizer
 
 __all__ = [
     'NewtonSchulzPseudoinverse',
     'HigherOrderNewtonSchulzPseudoinverse',
-    'QGMRESSolver'
+    'QGMRESSolver',
+    'Visualizer',
+    # Kernel/null space functions (exported via utils import *)
+    'quat_null_space',
+    'quat_null_right', 
+    'quat_null_left',
+    'quat_kernel'
 ]
 
